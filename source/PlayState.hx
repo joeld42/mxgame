ea3a48ec921f96f9ebac175328bc03b8c7c6752f
Joel Davis <joeld42@gmail.com>
2016-07-12 00:08:20 -0700

    bg tiles for underworld

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -78,13 +78,9 @@ class PlayState extends FlxState
 		add(player);
 
 		//player.attachSpriter( this, "assets/images/sampleplayer/", "assets/data/player.scml" );
-		//player.attachSpriter( this, "assets/images/robodog/", "assets/data/robodog.scml" );
-		// player.attachSpriter( this, "assets/images/tiger/", "assets/images/tiger/tiger.scml" );
-		// player.attachSpriter( this, "assets/images/magickitty/", "assets/images/magickitty/magickitty.scml" );
-		// player.attachSpriter( this, "assets/images/underworld/", "assets/images/underworld/ninjakitty.scml" );
-		//player.attachSpriter( this, "assets/images/cody/", "assets/images/cody/cody.scml" );
-		// player.attachSpriter( this, "assets/images/leopard/", "assets/images/leopard/leopard.scml" );
-		player.attachSpriter( this, "assets/images/lavapool/", "assets/images/lavapool/lavapool.scml" );
+		var charPath = 'assets/images/${info.options.charIdent}/';
+		player.attachSpriter( this, charPath, '${charPath}${info.options.charIdent}.scml' );		
+		
 
 		// FlxG.cameras.bgColor = new FlxColor( 0xd2e9fc );
 		FlxG.camera.follow( player, FlxCameraFollowStyle.PLATFORMER );
